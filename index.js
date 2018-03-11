8c073200807deac34f01e9178e60609003a96db0
Peter Seremetis <pswriter5348@gmail.com>
2018-03-11 22:43:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+   return document.querySelector(`${selector}`);
+}
+
+function nestedTarget(){
+   return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n){
+    const rankedLists = document.querySelectorAll('.ranked-list');
+       for(let i = 0; i < rankedLists.length; i++){
+          rankedLists[i].innerHTML = (`${i + 3}`);
+  }
+    return rankedLists
+}
+
+function deepestChild(){
+    return document.querySelector('#grand-node div div div div');
+}

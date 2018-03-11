fe27dcd144b1853a46caba10f8371c2ca9af267f
Peter Seremetis <pswriter5348@gmail.com>
2018-03-11 22:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function getFirstSelector(selector){
-return document.querySelector(`${selector}`)
+   return document.querySelector(`${selector}`)
 }
+
 function nestedTarget(){
-return document.querySelector(`#nested .target`)
+   return document.querySelector(`#nested .target`)
 }
+
 function increaseRankBy(n){
-  const rankedLists = document.querySelectorAll('.ranked-list');
-  for(let i = 0; i < rankedLists.length; i++){
-    rankedLists[i].innerHTML = (`${i + 3}`)
+    const rankedLists = document.querySelectorAll('.ranked-list');
+       for(let i = 0; i < rankedLists.length; i++){
+          rankedLists[i].innerHTML = (`${i + 3}`)
   }
-  return rankedLists
+    return rankedLists
 }
+
 function deepestChild(){
-  return document.querySelector('#grand-node div div div div')
-  //use querySelector and querySelectorAll
-  //Solution does not need to be generic
-  //not expected to work in every case.
+    return document.querySelector('#grand-node div div div div')
 }
-//use a similar technique to breadth-first search you were introduced to before
-//Mission: find the nodes

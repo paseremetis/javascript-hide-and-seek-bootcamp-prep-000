2f79385d7055524bb151fb84a9b7bcae6ecff53e
Peter Seremetis <pswriter5348@gmail.com>
2018-03-11 22:20:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+return document.querySelector(`${selector}`)
+}
+function nestedTarget(){
+return document.querySelector(`#nested .target`)
+}
+function increaseRankBy(n){
+   document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i < n.length; i++){
+    n[i].innerHTML = (i + `${n}`)
+  }
+  return n
+}
+function deepestChild(){
+  //use querySelector and querySelectorAll
+  //Solution does not need to be generic
+  //not expected to work in every case.
+}
+//use a similar technique to breadth-first search you were introduced to before
+//Mission: find the nodes

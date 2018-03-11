89e6924efd4be4af4607f0bf63936d4b74c15e73
Peter Seremetis <pswriter5348@gmail.com>
2018-03-11 22:24:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget(){
 return document.querySelector(`#nested .target`)
 }
 function increaseRankBy(n){
-   document.querySelectorAll('.ranked-list li');
+   document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for(let i = 0; i < n.length; i++){
     n[i].innerHTML = (i + `${n}`)
   }
